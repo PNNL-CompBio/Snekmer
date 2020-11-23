04d2bace6c7033a2d99f0ef6fa2b1af68162a5d3
christinehc <distant.blur@gmail.com>
2020-11-23 10:16:22 -0800

    Initialize files and parameters for pip install

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 with open('requirements.txt') as f:
     required = [x for x in f.read().splitlines() if not x.startswith("#")]
 
-pkgs = find_packages(exclude=('Util'))
+pkgs = find_packages(exclude=('test'))  # also Util?
 
 # Note: the _program variable is set in __init__.py.
 # it determines the name of the package/final command line tool.
@@ -14,8 +14,8 @@ pkgs = find_packages(exclude=('Util'))
 setup(name='kmerfeatures',
       version=__version__,
       packages=['kmerfeatures'],
-      test_suite='pytest.collector',
-      tests_require=['pytest'],
+      # test_suite='pytest.collector',
+      # tests_require=['pytest'],
       description=('Kmer pipeline for generating features and'
                    'running SIEVE models on input sequences)',
       url='http://github.com/biodataganache/KmerPipeline/',
