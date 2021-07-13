44ca552181835aa447d70971076345ab9d316583
christinehc <distant.blur@gmail.com>
2021-07-13 13:12:16 -0700

    Return figure and axis object instead of display w/o output

--- snekmer/plot.py
+++ snekmer/plot.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import KFold, train_test_split, RandomizedSearchCV,
 from sklearn.svm import SVC
 
 
-def show_cv_roc_curve(clf, cv, X, y, title='ROC Curve'):
+def show_cv_roc_curve(clf, cv, X, y, title='ROC Curve', ax=None, dpi=400):
     """Plot cross-validated receiver operator characteristics curve.
 
     Adapted from example in sklearn documentation [1].
@@ -29,12 +29,21 @@ def show_cv_roc_curve(clf, cv, X, y, title='ROC Curve'):
         Response dataframe.
     title : str
         Plot title.
+    ax : matplotlib.axes.Axes object or None (default: None)
+        Axis object, if already created.
+    dpi : int (default: 400)
+        Figure resolution.
 
     Returns
     -------
     None
 
     """
+    # initialize figure and define all axes
+    fig = plt.figure(dpi=dpi, figsize=(8, 4), facecolor='white')
+    ax = ax or plt.gca()
+
+    # create empty arrays to store data
     tprs, aucs = [], []
     mean_fpr = np.linspace(0, 1, 100)
 
@@ -48,42 +57,43 @@ def show_cv_roc_curve(clf, cv, X, y, title='ROC Curve'):
         tprs[-1][0] = 0.0
         roc_auc = auc(fpr, tpr)
         aucs.append(roc_auc)
-        plt.plot(fpr, tpr, lw=1, alpha=0.3,
-                 label=f"ROC fold {i} (AUC = {roc_auc:0.2f})")
+        ax.plot(fpr, tpr, lw=1, alpha=0.3,
+                label=f"ROC fold {i} (AUC = {roc_auc:0.2f})")
 
     # plot y = x (50% chance) reference line
-    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-             label='Chance', alpha=0.8)
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+            label='Chance', alpha=0.8)
 
     # calculate mean and stdev roc_auc and show in plot
     mean_tpr = np.mean(tprs, axis=0)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
     std_auc = np.std(aucs)
-    plt.plot(mean_fpr, mean_tpr, color='b',
-             label=(f"Mean ROC (AUC = {mean_auc:0.2f}"
-                    r" $\pm$ "
-                    f" {std_auc:0.2f})"),
-             lw=2, alpha=.8)
+    ax.plot(mean_fpr, mean_tpr, color='b',
+            label=(f"Mean ROC (AUC = {mean_auc:0.2f}"
+                   r" $\pm$ "
+                   f" {std_auc:0.2f})"),
+            lw=2, alpha=.8)
 
     # draw shaded area for +- 1 stdev from mean auc roc
     std_tpr = np.std(tprs, axis=0)
     tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
     tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-                     label=r'$\pm$ 1 std. dev.')
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.')
 
     # set fixed plot limits from ~0 to ~1
-    plt.xlim([-0.05, 1.05])
-    plt.ylim([-0.05, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title(title)
-    plt.legend(bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.)
-    plt.show()
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title(title)
+    ax.legend(bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.)
+
+    return fig, ax
 
 
-def show_cv_pr_curve(clf, cv, X, y, title='PR Curve'):
+def show_cv_pr_curve(clf, cv, X, y, title='PR Curve', ax=None, dpi=400):
     """Plot cross-validated precision-recall curve.
 
     Parameters
@@ -98,12 +108,21 @@ def show_cv_pr_curve(clf, cv, X, y, title='PR Curve'):
         Response dataframe.
     title : str
         Plot title.
+    ax : matplotlib.axes.Axes object or None (default: None)
+        Axis object, if already created.
+    dpi : int (default: 400)
+        Figure resolution.
 
     Returns
     -------
     None
 
     """
+    # initialize figure and define all axes
+    fig = plt.figure(dpi=dpi, figsize=(8, 4), facecolor='white')
+    ax = ax or plt.gca()
+
+    # create empty arrays to store data
     y_real, y_proba = [], []
 
     for i, (train, test) in enumerate(cv.split(X, y)):
@@ -113,8 +132,8 @@ def show_cv_pr_curve(clf, cv, X, y, title='PR Curve'):
         avg_precision = average_precision_score(y[test], probabilities[:, 1])
 
         # Plotting each individual PR Curve
-        plt.plot(recall, precision, lw=1, alpha=0.3,
-                 label=f"PR fold {i} (AUC = {avg_precision:0.2f})")
+        ax.plot(recall, precision, lw=1, alpha=0.3,
+                label=f"PR fold {i} (AUC = {avg_precision:0.2f})")
 
         y_real.append(y[test])
         y_proba.append(probabilities[:, 1])
@@ -126,15 +145,15 @@ def show_cv_pr_curve(clf, cv, X, y, title='PR Curve'):
     avg_total_precision = average_precision_score(y_real, y_proba)
 
     # plot average p-r curve
-    plt.plot(recall, precision, color='b',
-             label=f"Precision-Recall (AUC = {avg_total_precision:0.2f})",
-             lw=2, alpha=.8)
+    ax.plot(recall, precision, color='b',
+            label=f"Precision-Recall (AUC = {avg_total_precision:0.2f})",
+            lw=2, alpha=.8)
 
     # set fixed plot limits from ~0 to ~1
-    plt.xlim([-0.05, 1.05])
-    plt.ylim([-0.05, 1.05])
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.title(title)
-    plt.legend(loc="lower right")
-    plt.show()
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.set_title(title)
+    ax.legend(loc="lower right")
+    return fig, ax
