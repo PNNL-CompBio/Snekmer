67a3d432485b38e81325301abbd0277587edc2de
christinehc <distant.blur@gmail.com>
2021-09-27 09:49:34 -0700

    Correctly parse `alphabet=None` kmer label

--- snekmer/io.py
+++ snekmer/io.py
@@ -85,7 +85,7 @@ def read_output_kmers(filename):
             # parse kmer outputs if detected
             if re.findall(r'^KMER', line_data[0]):
                 kmers += line_data
-    prefix = re.search(r'^KMER-[\d]+-[A-Z]+-', kmers[0]).group()
+    prefix = re.search(r'^KMER-[\d]+-[A-Za-z]+-', kmers[0]).group()
     return [s.strip().replace(prefix, "") for s in kmers]
 
 
