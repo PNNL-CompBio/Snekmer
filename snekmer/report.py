e80c8651831e054d4afae3c988f06af935eafad0
abbyjerger <arosejerger@gmail.com>
2022-10-07 18:05:01 -0700

    Add file links for each csv output in the Snekmer Search report.

--- snekmer/report.py
+++ snekmer/report.py
@@ -70,11 +70,11 @@ def create_report(template_vars, template: str, report_file_name: str):
         f.write(html)
 
 
-# model or search
+# model
 def create_report_many_images(
     path: str, rep_vars: dict, template: str, report_file_name: str
 ):
-    """Create report for Snekmer model or search modes.
+    """Create report for Snekmer model mode.
 
     Parameters
     ----------
@@ -99,5 +99,31 @@ def create_report_many_images(
             if file.endswith(".png"):
                 filelist.append(os.path.join(root, file))
     rep_vars["images"] = [correct_rel_path(f) for f in filelist]
+    create_report(rep_vars, template, report_file_name)
+
+
+def create_report_many_csvs(
+    path: str, rep_vars: dict, template: str, report_file_name: str
+):
+    # create lists with directory names and file names
+    file_list = []
+    dirs_list = []
+    for root, dirs, files in os.walk(path):
+        for d in dirs:
+            dirs_list.append(d)
+        for file in files:
+            if file.endswith(".csv"):
+                file_list.append(os.path.join(root, file))
+
+    # combine both lists into one, in the correct order to print in the jinja template
+    ordered = []
+    for i in dirs_list:
+        ordered.append(i)
+        for j in file_list:
+            if os.path.split(os.path.split(j)[0])[1] == i:
+                ordered.append(j)
+
+    rep_vars['dirs_list'] = dirs_list
+    rep_vars['ordered'] = ordered
     create_report(rep_vars, template, report_file_name)
 
