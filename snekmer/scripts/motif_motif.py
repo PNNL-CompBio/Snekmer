af8d11d1ddc83da439fb9b8c4fd786460d578552
Tara Nitka <tara.nitka@pnnl.gov>
2023-05-01 12:16:09 -0700

    Fix issue with DataFrame not being converted to ndarray

--- snekmer/scripts/motif_motif.py
+++ snekmer/scripts/motif_motif.py
@@ -43,7 +43,7 @@ config = snakemake.config
     
 # load input data
 with open(snakemake.input.matrix, "rb") as f:
-    data = pickle.load(f)
+    data = pd.DataFrame.to_numpy(pickle.load(f))
     
 with open(snakemake.input.kmers, "rb") as f:
     kmers = f.readlines()
