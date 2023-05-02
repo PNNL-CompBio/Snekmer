4b55fce8ed587e2deab85bf4483ab0515988b0c1
Tara Nitka <tara.nitka@pnnl.gov>
2023-05-02 13:53:03 -0700

    Fix error tabulating permutation scores

--- snekmer/scripts/motif_motif.py
+++ snekmer/scripts/motif_motif.py
@@ -118,7 +118,7 @@ for i in range(n_iter):
         vec_col="sequence_vector",
         **config["score"]["scaler_kwargs"],)
     perm_scores = pd.DataFrame.from_dict(scorer.scores)
-    score_matrix.assign(perm_scores)
+    score_matrix.merge(perm_scores, left)
     
 output_matrix = motif.p_values(score_matrix, weights, n_iter)
     
