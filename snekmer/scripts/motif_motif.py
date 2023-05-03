1f4f6c4709b3c217a9b94b7c3a539d7514efc3e9
Tara Nitka <tara.nitka@pnnl.gov>
2023-05-03 10:49:04 -0700

    Fix array dimension error in motif script

--- snekmer/scripts/motif_motif.py
+++ snekmer/scripts/motif_motif.py
@@ -117,7 +117,7 @@ for i in range(n_iter):
         label_col=label,
         vec_col="sequence_vector",
         **config["score"]["scaler_kwargs"],)
-    perm_scores = pd.DataFrame.to_numpy(pd.DataFrame.from_records(scorer.scores))
+    perm_scores = pd.DataFrame.to_numpy(pd.DataFrame(scorer.scores))
     score_matrix=np.append(score_matrix, perm_scores, 1)
     
 output_matrix = motif.p_values(score_matrix, weights, n_iter)
