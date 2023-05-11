1cf25a442bcb5c0caa065a56db2d71a34dd7e59c
Tara Nitka <tara.nitka@pnnl.gov>
2023-05-11 08:19:34 -0700

    Add scores as output from motif

--- snekmer/scripts/motif_motif.py
+++ snekmer/scripts/motif_motif.py
@@ -121,7 +121,7 @@ else:
 output_matrix = motif.p_values(score_matrix, scores, n_iter)
     
 # save output
-# score_matrix.to_csv(snakemake.output.data, index=False, compression="gzip")
+score_matrix.to_csv(snakemake.output.data, index=False, compression="gzip")
 output_matrix.to_csv(snakemake.output.p_values, index=False, compression="gzip")
 
 # record script endtime
