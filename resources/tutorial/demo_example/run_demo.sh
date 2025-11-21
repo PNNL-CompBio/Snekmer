b352aa51dd0dcba4529c952a95cdbd6975c8f72e
Tara Nitka <tara.nitka@pnnl.gov>
2025-11-21 13:10:29 -0800

    Pin scikit-learn and remove motif from Learn/Apply tutorial

--- resources/tutorial/demo_example/run_demo.sh
+++ resources/tutorial/demo_example/run_demo.sh
@@ -18,6 +18,3 @@ mv output/scoring/*scorer output/example-model/
 
 # run snekmer search on examples using provided config.yaml
 snekmer search --configfile=../../config.yaml
-
-# run snekmer motif on examples using provided config.yaml
-snekmer motif --configfile=../../config.yaml
